dd6740b47ee37884c71855d55c85aa193c63d84d
Vladzio <dolynkavladzio@gmail.com>
2025-02-24 18:57:50 +0000

    Written test commands

--- .github/tests/cypress/e2e/app.cy.js
+++ .github/tests/cypress/e2e/app.cy.js
@@ -1,6 +1,20 @@
-describe('Demo  Web App Test', () => {
-  
+describe('ProvedCode infrasrtucture tests', () => {
   it('Start page successfully loads', () => {
     cy.visit('/');
+    // == Buttons =====================================
+    cy.get('button[name="Header_btn__dJRZu"]').click();
+    cy.get('button[name="AuthModal_header_btn__sPgN2"]').click();
+    // == Register info ===============================
+    cy.get('input[name="first_name"]').type('Vladzio');
+    cy.get('input[name="last_name"]').type('Dolynka');
+
+    cy.get('input[name="login"]').type('dolynkavladzio@gmail.com');
+
+    cy.get('input[name="specialization"]').type('DevOps');
+
+    cy.get('input[name="password"]').type('Programist');
+    cy.get('input[name="repeated_password"]').type('Programist');
+    // == Button ======================================
+    cy.get('button[name="Register_btn__Ctx1+"]').click();
   });
 });
