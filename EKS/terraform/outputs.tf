5ffe3f943922bd7754cca99bd006bca3a5fac331
Vladzio <dolynkavladzio@gmail.com>
2025-03-10 13:28:08 +0000

    Added new outputs

--- EKS/terraform/outputs.tf
+++ EKS/terraform/outputs.tf
@@ -2,26 +2,30 @@
 # Outputs
 #########################################################################
 output "cluster_name" {
-	value = local.name
+        value = local.name
 }
 #-----------------------------------------------------
 output "region" {
-	value = local.region
-}
-#-----------------------------------------------------
-output "vpc_id" {
-	value = module.vpc.vpc_id
+        value = local.region
 }
 #-----------------------------------------------------
 output "private_subnets" {
-	value = module.vpc.private_subnets
+        value = module.vpc.private_subnets
 }
 #-----------------------------------------------------
 output "public_subnets" {
-	value = module.vpc.public_subnets
+        value = module.vpc.public_subnets
 }
 #-----------------------------------------------------
 output "eks_cluster_id" {
-	value = module.eks.cluster_id
+        value = module.eks.cluster_id
+}
+#-----------------------------------------------------
+output "default_security_group_id" {
+        value = module.vpc.default_security_group_id
+}
+#-----------------------------------------------------
+output "intra_subnets" {
+        value = module.vpc.intra_subnets
 }
 #-----------------------------------------------------
